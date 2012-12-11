39b58cc1a79259cf8911def34c1c1e63b6a7b90c
Marijn Huizendveld <marijn.huizendveld@gmail.com>
2012-12-11 20:09:09 +0100

    Utilize an external library for UUID generation.

--- src/ass/XmlSecurity/DSig.php
+++ src/ass/XmlSecurity/DSig.php
@@ -45,6 +45,7 @@ namespace ass\XmlSecurity;
 
 use ass\XmlSecurity\Exception\InvalidArgumentException;
 use ass\XmlSecurity\Exception\MissingMandatoryParametersException;
+use Rhumsaa\Uuid\Uuid;
 
 /**
  * This class provides methods to digitally sign XML documents and implements
@@ -377,28 +378,11 @@ class DSig
     /**
      * Generate a pseudo-random version 4 UUID.
      *
-     * @see http://de.php.net/manual/en/function.uniqid.php#94959
-     *
      * @return string
      */
     public static function generateUUID()
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-        );
+        return Uuid::uuid4()->toString();
     }
 
     /**
